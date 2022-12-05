76067e574796d008021d7a64872873876600dbe3
Houston4444 <picotmathieu@gmail.com>
2022-12-05 18:33:18 +0100

    nicer way to open the editor help file

--- src/editor_help_dialog.py
+++ src/editor_help_dialog.py
@@ -1,8 +1,11 @@
+import logging
 from pathlib import Path
 from PyQt5.QtWidgets import QDialog, QVBoxLayout
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 import ui.editor_help
+
+_logger = logging.Logger(__name__)
 
 class EditorHelpDialog(QDialog):
     def __init__(self, parent):
@@ -11,8 +14,12 @@ class EditorHelpDialog(QDialog):
         self.ui.setupUi(self)
 
         path = Path(__file__).parent.parent / 'manual' / 'en' / 'editor_help.html'
-        file = open(path, 'r')
-        contents = file.read()
+        try:
+            with open(path, 'r') as file:
+                contents = file.read()
+        except Exception as e:
+            _logger(str(e))
+            return
         
         self.ui.webEngineView.setHtml(contents)
         
