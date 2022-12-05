2fe626dfc2dbf4ddd4945758abaa62471705fcbd
Houston4444 <picotmathieu@gmail.com>
2022-12-05 18:30:53 +0100

    fix editor_help path !

--- src/editor_help_dialog.py
+++ src/editor_help_dialog.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from PyQt5.QtWidgets import QDialog, QVBoxLayout
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
@@ -8,17 +9,10 @@ class EditorHelpDialog(QDialog):
         super().__init__(parent)
         self.ui = ui.editor_help.Ui_Dialog()
         self.ui.setupUi(self)
-        # vbox =  QVBoxLayout(self)
-        
-        path = '/home/houston/codes_sources/Patchichi/manual/en/editor_help.html'
-        
+
+        path = Path(__file__).parent.parent / 'manual' / 'en' / 'editor_help.html'
         file = open(path, 'r')
         contents = file.read()
         
-        # self.ui.textBrowser.setHtml(contents)
-        # self.ui.webView.set
-        # self._web_view = QWebEngineView(self)
-        # self._web_view.setHtml(contents)
-        # vbox.addWidget(self._web_view)
         self.ui.webEngineView.setHtml(contents)
         
