8dd9b5c823441189b8067815cbaf358e14281bac
Houston4444 <picotmathieu@gmail.com>
2024-09-05 21:56:27 +0200

    update patchbay

--- src/fast_test.py
+++ src/fast_test.py
@@ -3,81 +3,41 @@ import time
 from typing import Callable
 
 
-lala = True
-tata = ['chamo']
+class Pali:
+    def __init__(self, name: str) -> None:
+        self.name = name
 
-def normal_count():
-    for i in range(1000000):
-        if lala:
-            ...
+class Chani:
+    def __init__(self, pali: Pali):
+        self.pali = pali
 
-def count_is():
-    for i in range(1000000):
-        if lala is True:
-            ...
-
-def count_eq():
-    for i in range(1000000):
-        if lala == True:
-            ...
-
-def count_list_str():
-    for i in range(1000000):
-        if tata:
-            ...
-
-
-big_list = [(i, i+1) for i in range(1000000)]
-oth_big_list = [(i, i+1) for i in range(1000000)]
-big_set = set(big_list)
-
-
-def list_in_list():
-    for li in big_list:
-        li in oth_big_list
+    def __enter__(self):
+        print('cabouli')
+        return self.pali
         
-def list_in_set():
-    oth_big_set = set(oth_big_list)
-    for li in big_list:
-        li in oth_big_set
+    def __exit__(self, *args, **kwargs):
+        print('sortie')
 
-def parse_list():
-    for i, j in big_list:
+
+long_list = [i for i in range(1000000)]
+
+def direct_search():
+    i = 999999
+    if i in long_list:
         ...
-
-def parse_list_comp():
-    [(i, j) for i, j in big_list]
-
-def parse_set():
-    # big_set = set(big_list)
-    for i, j in big_set:
-        ...
-
-class Fa:
-    def __init__(self):
-        self._nimp = 'Ratata'
         
-    def get_nimp(self) -> str:
-        return self._nimp
-    
-fas = [Fa() for i in range(10000)]
-
-
-def list_attr():
-    for fa in fas:
-        if fa._nimp:
-            ...
-            
-def list_getter():
-    for fa in fas:
-        if fa.get_nimp():
-            ...
+def setted_search():
+    i = 999999
+    long_set = set(long_list)
+    if i in long_list:
+        ...
 
 def measure(callable: Callable):
     before = time.time()
     callable()
     after = time.time()
     print('tadam', after - before)
-    
-measure(list_attr)
-measure(list_getter)
+
+
+measure(direct_search)
+measure(setted_search)
