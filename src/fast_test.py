c43ec90e32b28dc5c6b9af857ae362adf30a2db1
Houston4444 <picotmathieu@gmail.com>
2024-10-13 09:56:10 +0200

    fast tests update

--- src/fast_test.py
+++ src/fast_test.py
@@ -1,6 +1,9 @@
 
 import time
-from typing import Callable
+from typing import Callable, Any
+import json
+from pathlib import Path
+import yaml
 
 class Pali:
     def __init__(self, name: str) -> None:
@@ -51,5 +54,94 @@ def kiki_is():
             ...
     
 
-measure(kiki_eq)
-measure(kiki_is)
+def from_json_to_str(input_dict: dict[str, Any]) -> str:
+    '''for a canvas json dict ready to be saved,
+    return a str containing the json contents with a 2 chars indentation
+    and xy pos grouped on the same line.'''
+
+    PATH_OPENING = 0
+    PATH_IN = 1
+    PATH_CLOSING = 2
+
+    json_str = json.dumps(input_dict, indent=2)
+    final_str = ''
+    
+    path = list[str]()
+    path_step = PATH_IN
+    
+    for line in json_str.splitlines():
+        strip = line.strip()
+        
+        if line.endswith(('{', '[')):
+            path_name = ''
+            if strip.startswith('"') and strip[:-1].endswith('": '):
+                path_name = strip[1:-4]
+
+            n_spaces = 0
+            for c in line:
+                if c != ' ':
+                    break
+                n_spaces += 1
+            
+            path = path[:(n_spaces // 2)]
+            path.append(path_name)
+            path_step = PATH_OPENING
+        
+        elif line.endswith(('],', ']', '},', '}')):
+            path_step = PATH_CLOSING
+        
+        else:
+            path_step = PATH_IN
+        
+        if len(path) >= 5 and path[1] == 'views':
+            if len(path) == 5:
+                if path_step in (PATH_OPENING, PATH_CLOSING):
+                    final_str += line
+                    final_str += '\n'
+            
+            elif len(path) == 6 and path[-1] == 'boxes':
+                if path_step == PATH_IN:
+                    final_str += line
+                    final_str += '\n'
+                    
+            else:
+                final_str += line
+                final_str += '\n'
+        # if len(path) 
+        else:
+            final_str += line
+            final_str += '\n'
+
+        if path_step == PATH_CLOSING:
+            path = path[:-1]
+
+    return final_str
+
+
+# measure(kiki_eq)
+# measure(kiki_is)
+dir = Path('/home/houstonlzk5/.local/share/Patchichi/scenes/')
+import yaml
+path = dir / 'palouxe_yaml.patchichi.json.yaml'
+with open(path, 'r') as f:
+    poka = yaml.safe_load(f)
+    
+print(poka['editor_text'])
+# for path in dir.iterdir():
+#     if not path.name ('.patchichi.json'):
+#         continue
+
+#     print(path)
+#     with open(path, 'r') as file:
+#         json_dict = json.load(file)
+        
+#     try:
+#         version = tuple(json_dict.get('VERSION'))
+#         if version >= (0, 2):
+#             string = from_json_to_str(json_dict)
+#             # print(string)
+#             with open(path, 'w') as file:
+#                 file.write(string)
+#     except BaseException as e:
+#         print('nonon')
+#         print(str(e))
